bedfee4b5d28b937a92f53813e884f8ec92a2abf
hobiJeong <jjb26433@naver.com>
2026-05-06 10:17:55 +0900

    feat(app): wire MikroORM + GraphQL + CQRS + Auth in AppModule

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,4 +1,29 @@
 import { Module } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { CqrsModule } from '@nestjs/cqrs';
+import mikroOrmConfig from './libs/orm/mikro-orm.config';
+import { GqlExceptionFilter } from './libs/exceptions/gql-exception.filter';
+import { AuthModule } from './features/auth/auth.module';
 
-@Module({})
+@Module({
+  imports: [
+    MikroOrmModule.forRoot(mikroOrmConfig),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      sortSchema: true,
+      context: ({ req }: any) => ({ req }),
+    }),
+    ThrottlerModule.forRoot([{ ttl: 15 * 60 * 1000, limit: 1000 }]),
+    CqrsModule.forRoot(),
+    AuthModule,
+  ],
+  providers: [
+    { provide: APP_FILTER, useClass: GqlExceptionFilter },
+  ],
+})
 export class AppModule {}
